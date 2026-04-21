9ec7352cc9e01f2e149c1124f3741f80f37fe964
davidnavarrete <davidnava1998efrain@outlook.com>
2026-04-20 21:57:26 -0500

    curso de angular

--- 04-contry-app/src/app/country/pages/by-capital-page.component/by-capital-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-by-capital-page',
-  imports: [],
-  templateUrl: './by-capital-page.component.html'
-})
-export class ByCapitalPageComponent { }
--- 04-contry-app/src/app/shared/pages/home-page/home-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
-@Component({
-  selector: 'app-home-page.component',
-  imports: [ RouterLink ],
-  templateUrl: './home-page.component.html',
-})
-export class HomePageComponent { }
